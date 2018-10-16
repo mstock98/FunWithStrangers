1ce78dc845844fb4b1a2d8eaee3b313ef43b5cf7
mstock98 <mstock@mtu.edu>
2018-10-16 11:59:58 -0400

    Created empty activity for Tic Tac Toe. Not sure if we want to complete opponentSearch before we start working on this.

--- app/src/main/java/shaftware/funwithstrangers/MainActivity.java
+++ app/src/main/java/shaftware/funwithstrangers/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-// This is a test comment, please ignore/remove me if you wish
 public class MainActivity extends AppCompatActivity {
     Button ticTacToeButton;
     @Override
