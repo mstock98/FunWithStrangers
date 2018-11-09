55f9a667bc9dc3ee12df1383e60561cafa2a4f8f
cthubbar <cthubbar@mtu.edu>
2018-11-08 20:54:30 -0500

    Multiplayer added changes to the way activities start to prepare for quality of life changes background color changes depending on whose turn it is in online mode

--- app/src/main/java/shaftware/funwithstrangers/titleScreen.java
+++ app/src/main/java/shaftware/funwithstrangers/titleScreen.java
@@ -1,5 +1,6 @@
 package shaftware.funwithstrangers;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,8 @@ import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
+import com.google.android.gms.nearby.Nearby;
+
 public class titleScreen extends AppCompatActivity {
 
     @Override
@@ -15,13 +18,11 @@ public class titleScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_screen);
 
-
         final Globals.Mode mode;
         Globals g = (Globals)getApplication();
         mode = g.getMode();
-
-
-        TextView title = findViewById(R.id.titleText);
+        TextView title = findViewById(R.
+                id.titleText);
         switch(mode){
             case CHECKERS:
                 title.setText("Checkers");
@@ -40,6 +41,7 @@ public class titleScreen extends AppCompatActivity {
         Button hostButton = findViewById(R.id.hostButton);
         hostButton.setOnClickListener(new OnClickListener() {
             public void onClick(View arg0) {
+                Globals.setHost(true);
                 Class next = titleScreen.class;
                 switch(mode){
                     case CHECKERS:
@@ -55,33 +57,33 @@ public class titleScreen extends AppCompatActivity {
                         //switch to HangMan activity, goes no where
                         break;
                 }
-
                 Intent myIntent = new Intent(titleScreen.this, next);
                 startActivity(myIntent);
             }
         });
 
-
         Button joinButton = findViewById(R.id.joinButton);
         joinButton.setOnClickListener(new OnClickListener() {
             public void onClick(View arg0) {
-
+                Globals.setHost(false);
                 //head back to title screen of game
                 Intent myIntent = new Intent(titleScreen.this, opponentSearch.class);
                 startActivity(myIntent);
             }
         });
 
-
         Button cancelButton = findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener(new OnClickListener() {
             public void onClick(View arg0) {
-
                 //head back to title screen of game
                 Intent myIntent = new Intent(titleScreen.this, MainActivity.class);
                 startActivity(myIntent);
             }
         });
-    }
 
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
 }
